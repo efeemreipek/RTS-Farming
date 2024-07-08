c93a7197c6a9b4114b4979991e4209af08e52dbf
Efe Emre İpek <ipekefeemre@gmail.com>
2024-07-08 14:37:31 +0300

    added resource regen

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    public static GameAssets Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Material goldMaterial;
+    public Material goldEmptyMaterial;
+}
--- Assets/Scripts/ResourceNode.cs
+++ Assets/Scripts/ResourceNode.cs
@@ -4,21 +4,58 @@ using UnityEngine;
 
 public class ResourceNode : Node
 {
-    private int maxGoldAmount = 5;
-    private int currentGoldAmount;
+    private int _maxGoldAmount = 5;
+    private int _currentGoldAmount;
+    private int _regenerationTime = 15;
 
     protected override void Start()
     {
         base.Start();
 
-        currentGoldAmount = maxGoldAmount;
+        SetCurrentToMax();
     }
 
     public void DecrementGoldAmount()
     {
-        currentGoldAmount = Mathf.Max(currentGoldAmount - 1, 0);
+        _currentGoldAmount = Mathf.Max(_currentGoldAmount - 1, 0);
+        if(_currentGoldAmount <= 0)
+        {
+            StartCoroutine(RegenerateResource());
+            ChangeMaterialOfVisual();
+        }
     }
-    public bool CanGatherResource() => currentGoldAmount > 0;
-    public int GetMaxGoldAmount() => maxGoldAmount;
-    public int GetCurrentGoldAmount() => currentGoldAmount;
+    public bool CanGatherResource() => _currentGoldAmount > 0;
+    public int GetMaxGoldAmount() => _maxGoldAmount;
+    public int GetCurrentGoldAmount() => _currentGoldAmount;
+    public void SetCurrentToMax() => _currentGoldAmount = _maxGoldAmount;
+    private IEnumerator RegenerateResource()
+    {
+        yield return new WaitForSeconds(_regenerationTime);
+        SetCurrentToMax();
+        ChangeMaterialOfVisual();
+    }
+    private void ChangeMaterialOfVisual()
+    {
+        Transform visualTransform = transform.GetChild(1);
+        List<MeshRenderer> visualMeshRendererList = new List<MeshRenderer>();
+        for(int i = 0; i < visualTransform.childCount; i++)
+        {
+            visualMeshRendererList.Add(visualTransform.GetChild(i).GetComponent<MeshRenderer>());
+        }
+
+        if (!CanGatherResource())
+        {
+            foreach(MeshRenderer renderer in visualMeshRendererList)
+            {
+                renderer.material = GameAssets.Instance.goldEmptyMaterial;
+            }
+        }
+        else
+        {
+            foreach (MeshRenderer renderer in visualMeshRendererList)
+            {
+                renderer.material = GameAssets.Instance.goldMaterial;
+            }
+        }
+    }
 }
